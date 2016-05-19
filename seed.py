e2f6157dd01f41999cb58b6a9c74ac763a58931d
Ashley Hsiao <aiyihsiao@gmail.com>
2016-05-19 14:53:43 -0700

    Created functions so that user id and restaurant id increment properly if data seeded previously

--- seed.py
+++ seed.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import func
 
-from model import City, Restaurant
+from model import City, User, Restaurant
 from model import connect_to_db, db
 
 from server import app
@@ -74,8 +74,33 @@ def load_restaurants(city):
         db.session.add(restaurant)
 
         # Commit to save changes
-        db.session.commit()   
+        db.session.commit()
 
+
+def set_val_user_id():
+    """Set value for the next user_id after seeding database"""
+
+    # Get the Max user_id in the database
+    result = db.session.query(func.max(User.user_id)).one()
+    max_id = int(result[0])
+
+    # Set the value for the next user_id to be max_id + 1
+    query = "SELECT setval('users_user_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
+
+
+def set_val_restaurant_id():
+    """Set value for the next restaurant_id after seeding database"""
+
+    # Get the Max restaurant_id in the database
+    result = db.session.query(func.max(Restaurant.user_id)).one()
+    max_id = int(result[0])
+
+    # Set the value for the next restaurant_id to be max_id + 1
+    query = "SELECT setval('restaurants_restaurant_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
 
 
 # This makes a runtime error
@@ -156,7 +181,7 @@ if __name__ == "__main__":
     db.create_all()
 
     # Import different types of data
-    # load_restaurants()
+    load_restaurants()
     # load_categories()
-    # load_ratings()
-    # set_val_user_id()
+    set_val_user_id()
+    set_val_restaurant_id()
