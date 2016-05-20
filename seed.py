40073914227212e943018a6d3f12c77a29332034
Ashley Hsiao <aiyihsiao@gmail.com>
2016-05-20 14:58:10 -0700

    Removed set_val_user_id() as no user data seeded and function is unnecessary

--- seed.py
+++ seed.py
@@ -2,27 +2,12 @@
 
 from sqlalchemy import func
 
-from model import User, Restaurant
+from model import Restaurant
 from model import connect_to_db, db
 
 from server import app
 
 from yelp_api_call import load_restaurants
-
-
-# Might not need this function, as no user data seeded into database initially
-# Keep in database for now
-# def set_val_user_id():
-#     """Set value for the next user_id after seeding database"""
-
-#     # Get the Max user_id in the database
-#     result = db.session.query(func.max(User.user_id)).one()
-#     max_id = int(result[0])
-
-#     # Set the value for the next user_id to be max_id + 1
-#     query = "SELECT setval('users_user_id_seq', :new_id)"
-#     db.session.execute(query, {'new_id': max_id + 1})
-#     db.session.commit()
 
 
 def set_val_restaurant_id():
@@ -50,5 +35,4 @@ if __name__ == "__main__":
 
     # Import different types of data
     load_restaurants("Sunnyvale")
-    # set_val_user_id()
     set_val_restaurant_id()
