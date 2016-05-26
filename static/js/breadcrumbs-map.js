45520bb1382d02ed5d4ef55d56d10c252bf1fdbf
Ashley Hsiao <aiyihsiao@gmail.com>
2016-05-26 01:16:12 -0700

    Fixed up indentation and comments

--- static/js/breadcrumbs-map.js
+++ static/js/breadcrumbs-map.js
@@ -3,91 +3,78 @@
 // Define global variable so URL with user id for JSON can be passed into AJAX get request
 var user_visits_json = "/users/" + $("#user-info").data("userid") + "/visits.json";
 
-// Initialize Google Map
+// Initialize map
 function initMap() {
   
-  // Specify where the map is centered
-  // Note re: coordinates
-  // North is positive / South is negative
-  // West is negative / East is positive
-  // TODO: Center the map based on user's city (get coordinates for city)
-  var myLatLng = {lat: 37.3688, lng: -122.0363};
+    // Specify where the map is centered
+    // Re: coordinates - North is positive / South is negative / West is negative / East is positive
+    // TODO: Center the map based on user's city (get coordinates for city)
+    // Currently map center is hard coded with the coordinates for Sunnyvale
+    var myLatLng = {lat: 37.3688, lng: -122.0363};
 
-  // Create a map object and specify the DOM element for display
-  // Customize the control options
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 13,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-    },
-    streetViewControl: true,
-    streetViewControlOptions: {
-        position: google.maps.ControlPosition.LEFT_CENTER
-    },
-    zoomControl: true,
-    zoomControlOptions: {
-      position: google.maps.ControlPosition.LEFT_CENTER
-    },
-  });
+    // Create a map object and specify the DOM element for display
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 13,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        },
+        streetViewControl: true,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.LEFT_CENTER
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_CENTER
+        },
+    });
 
-  // Resource used for multiple info windows:
-  // http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
-  
-  // Create an info window object
-  // Note: This works whether placed inside or outside of the for loop
-  // When inside the for loop, object is recreated each time???
-  // But we just want the content of the info window to change
-  // Which works using "this" in the event handler
-  var infoWindow = new google.maps.InfoWindow({
-    maxWidth: 200
-  });
+    // Resource used for multiple info windows:
+    // http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
 
-  // Make AJAX call to server to get JSON that has info re: user's visits
-  // For every restaurant that the user has visited, place markers on map
-  // with an info window for each marker when clicked
-  $.get(user_visits_json, function (visits) {
+    // Create a global variable for the info window within this function
+    // This way, it changes the content each time when a marker is clicked
+    var infoWindow = new google.maps.InfoWindow({
+        maxWidth: 200
+    });
 
-    for (var key in visits) {
-      var visit = visits[key];
+    // Make AJAX call to server to get JSON that has info re: user's restaurant visits
+    // For every restaurant that the user has visited, place markers on map and display
+    // an info window when a marker is clicked
+    $.get(user_visits_json, function (visits) {
 
-      // Content of info window 
-      // Display information about the restaurant
-      var contentString = '<div id="content">' +
-        '<h3>'+ visit.restaurant + '</h3>' +
-        'Address: ' + visit.address + '<br>' +
-        'Phone: ' + visit.phone +
-        '</div>';
+        for (var key in visits) {
+            var visit = visits[key];
 
-      // This works whether placed inside or outside of the for loop
-      // Decided to place outside of the for loop (above)
-      // // Create an info window object
-      // var infowindow = new google.maps.InfoWindow({
-      //   maxWidth: 200
-      // });
+            // Content of info window 
+            // Display information about the restaurant
+            var contentString = '<div id="content">' +
+                '<h3>'+ visit.restaurant + '</h3>' +
+                'Address: ' + visit.address + '<br>' +
+                'Phone: ' + visit.phone +
+                '</div>';
 
-      // Specify marker coordinates with restaurant's coordinates
-      var markerLatLng = {lat: visit.latitude, lng: visit.longitude};
+            // Specify marker coordinates with restaurant's coordinates
+            var markerLatLng = {lat: visit.latitude, lng: visit.longitude};
 
-      // Create a marker object
-      var marker = new google.maps.Marker({
-        position: markerLatLng,
-        map: map,
-        title: 'Restaurant: ' + visit.restaurant,
-        html: contentString
-      });
+            // Create a marker object
+            var marker = new google.maps.Marker({
+                position: markerLatLng,
+                map: map,
+                title: 'Restaurant: ' + visit.restaurant,
+                html: contentString
+            });
 
-      // Add an event handler that sets content of info window to "this" 
-      // marker being clicked and open the info window for "this" marker
-      marker.addListener('click', function() {
-        infoWindow.setContent(this.html);
-        infoWindow.open(map, this);
-      });
+            // Add an event handler that sets content of info window to "this" 
+            // marker being clicked and open the info window for "this" marker
+            marker.addListener('click', function() {
+                infoWindow.setContent(this.html);
+                infoWindow.open(map, this);
+            });
 
-    }
+        }
 
-  });
-
+    });
   
 }
