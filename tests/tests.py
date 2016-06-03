4af4254cd0c9f30ec0e4ad0ccef5d0b523d24bdd
Ashley Hsiao <aiyihsiao@gmail.com>
2016-06-03 15:18:58 -0700

    Initial commit

---
+++ tests/tests.py
@@ -0,0 +1,127 @@
+"""Integration Testing Flask app"""
+
+# To run tests.py from anywhere, if in parent directory and want to run python tests/tests.py
+# and import modules from parent directory, i.e. server
+import os
+import sys
+
+current_file_path = os.path.realpath(__file__)
+current_dir = os.path.dirname(current_file_path)
+parent_dir = os.path.dirname(current_dir)
+
+sys.path.append(parent_dir)
+
+import server
+from server import app
+
+from unittest import TestCase
+
+
+# class FlaskTestsBasic(TestCase):
+#     """Flask tests."""
+
+#     def setUp(self):
+#         """Stuff to do before every test."""
+
+#         # Get the Flask test client
+#         self.client = app.test_client()
+
+#         # Show Flask errors that happen during tests
+#         app.config['TESTING'] = True
+
+#     def test_index(self):
+#         """Test homepage page."""
+
+#         result = self.client.get("/")
+#         self.assertEqual(result.status_code, 200)
+#         self.assertIn("The ultimate social media network for foodies", result.data)
+
+
+# class FlaskTestsDatabase(TestCase):
+#     """Flask tests that use the database."""
+
+#     def setUp(self):
+#         """Stuff to do before every test."""
+
+#         # Get the Flask test client
+#         self.client = app.test_client()
+#         app.config['TESTING'] = True
+
+#         # Connect to test database
+#         connect_to_db(app, "postgresql:///testdb")
+
+#         # Create tables and add sample data
+#         db.create_all()
+#         example_data()
+
+#     def tearDown(self):
+#         """Do at end of every test."""
+
+#         db.session.close()
+#         db.drop_all()
+
+#     def test_departments_list(self):
+#         """Test departments page."""
+
+#         result = self.client.get("/departments")
+#         self.assertIn("Legal", result.data)
+
+
+#     def test_departments_details(self):
+#         """Test departments page."""
+
+#         result = self.client.get("/department/fin")
+#         self.assertIn("Phone: 555-1000", result.data)
+
+
+#     def test_login(self):
+#         """Test login page."""
+
+#         result = self.client.post("/login", 
+#                                   data={"user_id": "rachel", "password": "123"},
+#                                   follow_redirects=True)
+#         self.assertIn("You are a valued user", result.data)
+
+
+# class FlaskTestsLoggedIn(TestCase):
+#     """Flask tests with user logged in to session."""
+
+#     def setUp(self):
+#         """Stuff to do before every test."""
+
+#         app.config['TESTING'] = True
+#         app.config['SECRET_KEY'] = 'key'
+#         self.client = app.test_client()
+
+#         with self.client as c:
+#             with c.session_transaction() as sess:
+#                 sess['user_id'] = 1
+
+#     def test_important_page(self):
+#         """Test important page."""
+
+#         result = self.client.get("/important")
+#         self.assertIn("You are a valued user", result.data)
+
+
+# class FlaskTestsLoggedOut(TestCase):
+#     """Flask tests with user logged in to session."""
+
+#     def setUp(self):
+#         """Stuff to do before every test."""
+
+#         app.config['TESTING'] = True
+#         self.client = app.test_client()
+
+#     def test_important_page(self):
+#         """Test that user can't see important page when logged out."""
+
+#         result = self.client.get("/important", follow_redirects=True)
+#         self.assertNotIn("You are a valued user", result.data)
+#         self.assertIn("You must be logged in", result.data)
+
+
+if __name__ == "__main__":
+    import unittest
+
+    unittest.main()
